185be019626489532c0238b02eddfc264b59793b
Ganesh <ganeshgarje716@gmail.com>
2026-06-28 12:35:25 +0530

    add annotations on class @RestController and @RequestMapping("/api/v1/") in the Class Add EmployeeService DI with @AutoWired Annotation

--- Spring-RestAPI-Employee/src/main/java/com/ganesh/rest/EmployeeRestController.java
+++ Spring-RestAPI-Employee/src/main/java/com/ganesh/rest/EmployeeRestController.java
@@ -1,5 +1,16 @@
 package com.ganesh.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ganesh.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class EmployeeRestController {
 
+	
+	@Autowired
+	EmployeeService employeeService;
 }
