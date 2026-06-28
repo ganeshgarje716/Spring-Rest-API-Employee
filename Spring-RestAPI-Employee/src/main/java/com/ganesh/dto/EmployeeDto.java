e57b70eb1c4df76fd81f7b71f91543fb95bbf206
Ganesh <ganeshgarje716@gmail.com>
2026-06-28 12:07:08 +0530

    Add Variables and Validations in EmployeeDto Class

--- Spring-RestAPI-Employee/src/main/java/com/ganesh/dto/EmployeeDto.java
+++ Spring-RestAPI-Employee/src/main/java/com/ganesh/dto/EmployeeDto.java
@@ -1,5 +1,28 @@
 package com.ganesh.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class EmployeeDto {
+	
+	
+	
+	@NotNull(message = "Name Is Required")
+	private String name;
+	
+	@NotNull(message = "Salary Is Required")
+	private Double salary;
+	
+	@NotNull(message = "Department Is Required")
+	private String department;
+	
+	@NotNull(message = "Address Is Required")
+	private String address;
+	
+	@NotNull(message = "Mobile No Is Required")
+	private String mobileNo;
+	
+	
 
 }
